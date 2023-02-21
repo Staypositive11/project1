7e9e213d0c6e8d3fc2a455eaa2ed69e494d096c7
Fedor <zzee.ga@icloud.com>
2023-02-21 16:30:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// "use sctrict",
+
+// alert ('Здарова,лох');
+
+// const result =confirm('Ты долбаёб?');
+// console.log(result);
+
+// const answer = prompt('Тебе 18','18');
+// console.log(answer +5);
+
+
+// const answers =[];
+
+// answers[0]= prompt('Ваше имя?','');
+// answers[1]= prompt('Ваша фамилия?','');
+// answers[2]= prompt('Ваш возраст?','');
+
+
+// console.log(typeof(null));
+
+// const category ="toys";
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user ='Ivan';
+
+// alert(`Привет,${user}`)
+
+
+// "use strict ";
+
+// console.log('arr'+'-object');
+
+// console.log(4 + +'5');
+
+let incr =10,
+    decr=10;
+// incr++;
+// decr--;
+
+console.log(--incr);
+console.log(++decr);
+
+console.log(5%2);
+
+console.log(2*4 === '8');
+
+
+
+
+const isCheked = true,
+    isClosed = true;
+
+console.log(isCheked&&isClosed)
